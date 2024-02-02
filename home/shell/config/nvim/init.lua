0e7340d51c1168caaaefa3793bb3833e6b7cf4ea
Adam Curtis <adam.curtis.dev@gmail.com>
2024-02-01 22:51:03 -0500

    nvim: add quickfix keybind to jump to item without focusing

--- home/shell/config/nvim/init.lua
+++ home/shell/config/nvim/init.lua
@@ -5,6 +5,15 @@ vim.cmd.source("$HOME/.vimrc")
 vim.keymap.set("n", "<Space>", "<Nop>")
 vim.g.mapleader = " "
 
+-- quickfix keybindings
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "qf",
+  callback = function(args)
+    -- jump to quickfix item without moving focus from the quickfix window location 
+    vim.keymap.set('n', '<Tab>', "<CR><C-W>p", { buffer = args.buf, noremap = true })
+  end
+})
+
 -- load plugins
 require('plugins')
 
