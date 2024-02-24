59048f21f311e0960e0d9e0ea50a82bc483c784c
Adam Curtis <adam.curtis.dev@gmail.com>
2024-02-24 15:57:05 -0500

    adjust minimal jump setting for beacon nvim

--- home/shell/config/nvim/init.lua
+++ home/shell/config/nvim/init.lua
@@ -6,6 +6,7 @@ vim.keymap.set("n", "<Space>", "<Nop>")
 vim.g.mapleader = " "
 
 -- beacon settings
+vim.g.beacon_minimal_jump = 5
 vim.g.beacon_ignore_filetypes = {"NvimTree", "qf"}
 vim.g.beacon_shrink = 0
 -- vim.g.beacon_fade = 0
