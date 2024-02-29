197fe47b489b36dfa9918fcff964789683453616
Adam Curtis <adam.curtis.dev@gmail.com>
2024-02-28 19:33:20 -0500

    add skhd

--- home/darwin/packages.nix
+++ home/darwin/packages.nix
@@ -1,7 +1,16 @@
-
 { pkgs, lib, ... }:
 {
   home.packages = with pkgs; [
     dockutil
+    skhd
   ];
+  home.activation.installSkhd = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    ${pkgs.skhd}/bin/skhd --install-service && \
+    ${pkgs.skhd}/bin/skhd --start-service
+  '';
+   
+  xdg.configFile."skhd/skhdrc" = {
+    source = ./config/skhdrc;
+  };
+
 }
