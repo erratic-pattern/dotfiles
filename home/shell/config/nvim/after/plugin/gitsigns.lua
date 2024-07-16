9b625099d5e6eaa2c3f7e761fe8f365a4b343f82
Adam Curtis <adam.curtis.dev@gmail.com>
2024-07-15 23:20:44 -0400

    improve gitsigns keybinds

--- home/shell/config/nvim/after/plugin/gitsigns.lua
+++ home/shell/config/nvim/after/plugin/gitsigns.lua
@@ -3,16 +3,15 @@ require 'gitsigns'.setup {
         local gs = package.loaded.gitsigns
         local opts = { noremap = true, buffer = bufnr }
 
-        vim.keymap.set('n', '<leader>gsb', gs.toggle_current_line_blame, opts)
-        vim.keymap.set('n', '<leader>gsB', gs.blame, opts)
-        vim.keymap.set('n', "<leader>gss", gs.stage_hunk, opts)
-        vim.keymap.set('n', "<leader>gsr", gs.reset_hunk, opts)
-        vim.keymap.set('n', "<leader>gsS", gs.stage_buffer, opts)
-        vim.keymap.set('v', '<leader>gss', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end, opts)
-        vim.keymap.set('v', '<leader>gsr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end, opts)
-        vim.keymap.set('n', "<leader>gsu", gs.undo_stage_hunk, opts)
-        vim.keymap.set('n', "<leader>gsd", gs.diffthis, opts) -- Diff in seperate buffer
-        -- vim.keymap.set('n', "<leader>gsd", function()   -- Inline diff
+        vim.keymap.set('n', '<leader>gb', gs.blame_line, opts)
+        vim.keymap.set('n', '<leader>gB', gs.blame, opts)
+        vim.keymap.set('n', "<leader>ghs", gs.stage_hunk, opts)
+        vim.keymap.set('n', "<leader>ghr", gs.reset_hunk, opts)
+        vim.keymap.set('v', '<leader>ghs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end, opts)
+        vim.keymap.set('v', '<leader>ghr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end, opts)
+        vim.keymap.set('n', "<leader>ghu", gs.undo_stage_hunk, opts)
+        vim.keymap.set('n', "<leader>gd", gs.diffthis, opts) -- Diff in seperate buffer
+        -- vim.keymap.set('n', "<leader>gd", function()   -- Inline diff
         --     gs.toggle_deleted()
         --     gs.toggle_linehl()
         -- end, opts)
