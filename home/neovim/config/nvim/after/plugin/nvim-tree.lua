ad6cf72eb1ed1080146ff07a93118d888ac07abb
Adam Curtis <adam.curtis.dev@gmail.com>
2024-07-20 16:28:55 -0400

    simplify nvim-tree keybinds with NvimTreeFindFileToggle

--- home/neovim/config/nvim/after/plugin/nvim-tree.lua
+++ home/neovim/config/nvim/after/plugin/nvim-tree.lua
@@ -2,6 +2,5 @@ require('nvim-tree').setup {
 	disable_netrw = false,
 }
 
-vim.keymap.set('n', '<leader>ex', '<cmd>NvimTreeFindFile<cr>')
-vim.keymap.set('n', '<leader>t', '<cmd>NvimTreeToggle<cr>')
+vim.keymap.set('n', '<leader>t', '<cmd>NvimTreeFindFileToggle<cr>')
 
