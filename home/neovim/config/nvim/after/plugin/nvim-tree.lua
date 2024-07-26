c32c2884eefde949b4a5260f97c9ecaccf03af0c
Adam Curtis <adam.curtis.dev@gmail.com>
2024-07-26 17:37:19 -0400

    split nvim-tree keybinds into toggle and find file

--- home/neovim/config/nvim/after/plugin/nvim-tree.lua
+++ home/neovim/config/nvim/after/plugin/nvim-tree.lua
@@ -2,5 +2,8 @@ require('nvim-tree').setup {
 	disable_netrw = false,
 }
 
-vim.keymap.set('n', '<leader>t', '<cmd>NvimTreeFindFileToggle<cr>',
-	{ noremap = true, desc = "Open file tree on current file (NvimTreeFindFileToggle)" })
+vim.keymap.set('n', '<leader>t', '<cmd>NvimTreeFindFile<cr>',
+	{ noremap = true, desc = "Open file tree on current file (NvimTreeFindFile)" })
+
+vim.keymap.set('n', '<leader>T', '<cmd>NvimTreeToggle<cr>',
+	{ noremap = true, desc = "Open/close file tree (NvimTreeToggle)" })
