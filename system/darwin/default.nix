04bdd20c51bb4f3603efdf82ba7e5f32ea9df5f2
Adam Curtis <adam.curtis.dev@gmail.com>
2025-01-29 20:05:13 +1300

    refactor: reorganize darwin default modules

--- system/darwin/default.nix
+++ system/darwin/default.nix
@@ -1,10 +1,11 @@
 { ... }:
 {
   imports = [
-    ./shell.nix
     ./fonts.nix
-    ./homebrew.nix
     ./home-manager.nix
+    ./homebrew.nix
+    ./packages.nix
+    ./shell.nix
     # ./spotlight-fix.nix
     ./system-settings.nix
     ./user.nix
