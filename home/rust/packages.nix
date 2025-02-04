b148dd1db957fe8334569c97b1699a6f622872e7
Adam Curtis <adam.curtis.dev@gmail.com>
2025-02-05 11:01:49 +1300

    rust: install cargo applications with Nix

--- home/rust/packages.nix
+++ home/rust/packages.nix
@@ -1,7 +1,34 @@
-{ pkgs, lib, ... }: {
+{ pkgs, lib, ... }:
+let
+  inherit (lib) listToAttrs;
+  inherit (builtins) baseNameOf unsafeDiscardStringContext;
+in
+{
   home.packages = with pkgs; [
     rustup
   ];
+
+  # Install cargo utilities into ~/.cargo/bin
+  home.file = listToAttrs
+    (
+      map
+        (p: {
+          name = ".cargo/bin/${baseNameOf (unsafeDiscardStringContext p)}";
+          value = { source = p; };
+        })
+        (with pkgs; [
+          "${cargo-binstall}/bin/cargo-binstall"
+          "${cargo-deny}/bin/cargo-deny"
+          "${cargo-hakari}/bin/cargo-hakari"
+          "${cargo-insta}/bin/cargo-insta"
+          "${cargo-nextest}/bin/cargo-nextest"
+          "${cargo-sweep}/bin/cargo-sweep"
+          "${cargo-udeps}/bin/cargo-udeps"
+          "${critcmp}/bin/critcmp"
+          "${sqlx-cli}/bin/cargo-sqlx"
+          "${sqlx-cli}/bin/sqlx"
+          "${taplo}/bin/taplo"
+        ]));
 
   home.sessionPath = [
     "$HOME/.cargo/bin"
