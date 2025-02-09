c39fac4c902676d695681a661dd753a2e720197a
Adam Curtis <adam.curtis.dev@gmail.com>
2025-02-09 17:58:12 +1300

    home: fix directory loading script

--- home/directories.nix
+++ home/directories.nix
@@ -1,7 +1,7 @@
 { lib, ... }:
 {
   home.activation.directories = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    mkdir -p ~/Notes
-    mkdir -p ~/Code
+    mkdir -p "$HOME/Code"
+    mkdir -p "$HOME/Notes"
   '';
 }
