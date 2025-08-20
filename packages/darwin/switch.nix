a4da7f3cc47b28f047dd41f57911d33dfea7557e
Adam Curtis <adam.curtis.dev@gmail.com>
2025-08-20 19:06:50 -0400

    flake: switch script improvements for sudo handling and hostname lookup

--- packages/darwin/switch.nix
+++ packages/darwin/switch.nix
@@ -12,12 +12,25 @@ writeShellApplication {
     darwin.packages.${system}.darwin-rebuild
   ];
   text = ''
+    # use sudo askpass in non-interactive session
+    if ! [ -t 0 ] && ! [ -t 1 ] && [ -n "$SUDO_ASKPASS" ]; then
+        SUDO_OPTS='-A'
+    else
+        SUDO_OPTS='''
+    fi
+
+    # find configuration name
     if [ -n "''${1-}" ]; then
         NAME="$1"
         shift
     else
-        NAME="$(scutil --get LocalHostName)"
+        HOSTNAME="$(scutil --get LocalHostName)"
+        echo "Finding configuration using local hostname: $HOSTNAME"
+        NAME="$(\
+          nix eval --raw ${self}#darwinConfigurations \
+          --apply "configs: with builtins; head (filter (name: configs.\''${name}.config.networking.hostName == \"$HOSTNAME\") (attrNames configs))")"
+        echo "Found configuration: $NAME"
     fi
-    sudo darwin-rebuild switch --flake "${self}#$NAME" "$@"
+    sudo $SUDO_OPTS darwin-rebuild switch --flake "${self}#$NAME" "$@"
   '';
 }
