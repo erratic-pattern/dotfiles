59490e8d8db571b777aff4ce6b1778d42b43b1ec
Adam Curtis <adam.curtis.dev@gmail.com>
2025-08-20 19:06:32 -0400

    syncthing: declarative device/folder settings

--- home/syncthing/default.nix
+++ home/syncthing/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   home.packages = with pkgs; [
     syncthing
@@ -6,5 +6,30 @@
 
   services.syncthing = {
     enable = true;
+
+    settings = {
+      devices = {
+        alexandria = {
+          id = "DNKOF4K-O6MBCKU-YYGMS7L-2TGO5C6-GKT7IEC-6RLVIWD-LPVBHQZ-R7Y7VQS";
+        };
+        SM-S9228U1 = {
+          id = "G7TMR43-HV5HYVA-V3UTMKP-RUMNQBM-J6AT7GK-QT7EV2C-SJ344AG-S5MDXQK";
+        };
+      };
+
+      # Folder configuration
+      folders = {
+        "books" = {
+          path = "${config.home.homeDirectory}/Books";
+          id = "books-sync";
+          devices = [ "alexandria" ]; # Share with alexandria
+        };
+        "notes" = {
+          path = "${config.home.homeDirectory}/Notes";
+          id = "rzdgt-rvrfr";
+          devices = [ "SM-S9228U1" ]; # Share with SMS9228U1
+        };
+      };
+    };
   };
 }
